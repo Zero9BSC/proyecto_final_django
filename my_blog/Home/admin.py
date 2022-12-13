3e586422a947fb658fefdebc68f8ee99aeb8f22a
Zero9BSC <franconicolasjones@gmail.com>
2022-12-13 20:15:46 -0300

    Fix categories and add README video link

--- my_blog/Home/admin.py
+++ my_blog/Home/admin.py
@@ -3,5 +3,4 @@ from .models import *
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Contacto)
-admin.site.register(Autor)
 admin.site.register(Perfil)
--- my_blog/Home/models.py
+++ my_blog/Home/models.py
@@ -9,30 +9,9 @@ CATEGORIES_CHOICES = (
     ("Noticias", "Noticias"),
     ("Reviews", "Reviews"),
     ("Gaming", "Gaming"),
-    ("Sofrware", "Sofrware"),
+    ("Software", "Software"),
     ("Hardware", "Hardware"),
 )
-
-
-
-class Autor(models.Model):
-    id = models.AutoField(primary_key = True)
-    nombres = models.CharField('Nombres de Autor',max_length = 255, null = False, blank = False)
-    apellidos = models.CharField('Apellidos de autor', max_length = 255, null = False, blank = False)
-    facebook = models.URLField('Facebook', null = True, blank = True)
-    twitter = models.URLField('Twitter', null = True, blank = True)
-    instagram = models.URLField('Instagram', null = True, blank = True)
-    web = models.URLField('Web', null = True, blank = True)
-    correo = models.EmailField('Correo Electrónico', blank = False, null = False)
-    estado = models.BooleanField('Autor Activo/No Activo',default = True)
-    fecha_creacion = models.DateField('Fecha de Creación', auto_now = False, auto_now_add = True)
-
-    class Meta:
-        verbose_name = 'Autor'
-        verbose_name_plural = 'Autores'
-
-    def __str__(self):
-        return "{0},{1}".format(self.apellidos, self.nombres)
 
 class Perfil(models.Model):
     #id = models.AutoField(primary_key = True)
